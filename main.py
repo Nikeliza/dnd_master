4eef9b433df175c8bf57fc04c383368e17e08d48
Nikeliza <nikeliza@mail.ru>
2020-10-14 19:30:44 +0300

    Update main.py
    Добавлен вывод играющих героев, обработка нажатия и двойного клика на героя

--- main.py
+++ main.py
@@ -21,6 +21,11 @@ class Heroes():
             mas_hero.append(i['name'])
         return mas_hero
 
+    def get_info_hero_for_name(self, name):
+        for i in range(len(self.mas_hero_info['heros'])):
+            if self.mas_hero_info['heros'][i]['name'] == name:
+                return self.mas_hero_info['heros'][i]
+
 class Monsters():
 
     def __init__(self):
@@ -63,7 +68,7 @@ class SettingsWindow(Toplevel):
 
         self.lbl_monsters = Label(self, text="Монстры", font=("Arial", 18))
         self.lbl_monsters.grid(column=3, row=0)
-        self.lbl_selected_monsters = Label(self, text=self.get_selected(self.selected_monsters), font=("Arial", 14))
+        self.lbl_selected_monsters = Label(self, text=self.get_selected_monsters(), font=("Arial", 14))
         self.lbl_selected_monsters.grid(column=3, row=2)
 
 
@@ -78,7 +83,7 @@ class SettingsWindow(Toplevel):
 
         self.lbl_heroes = Label(self, text="Герои", font=("Arial", 18))
         self.lbl_heroes.grid(column=0, row=0)
-        self.lbl_selected_heroes = Label(self, text=self.get_selected(self.selected_heroes), font=("Arial", 14))
+        self.lbl_selected_heroes = Label(self, text=self.get_selected_heroes(), font=("Arial", 14))
         self.lbl_selected_heroes.grid(column=0, row=2)
 
         self.btn_add_delete_heroes = Button(self, text="Добавить/удалить", command=self.clicked_add_delete_heroes)
@@ -96,20 +101,27 @@ class SettingsWindow(Toplevel):
         else:
             self.selected_monsters[self.combo_monsters.get()] = self.spin_monsters.get()
 
-    def get_selected(self, thing):
+    def get_selected_heroes(self):
         result_str = ''
-        for i in thing:
+        for i in self.selected_heroes:
             result_str += i + '\n'
+
+        return result_str
+
+    def get_selected_monsters(self):
+        result_str = ''
+        for i in self.selected_monsters:
+            result_str += i + ' ' + str(self.selected_monsters[i]) + '\n'
 
         return result_str
 
     def clicked_add_delete_heroes(self):
         self.update_selected_heroes()
-        self.lbl_selected_heroes.configure(text=self.get_selected(self.selected_heroes))
+        self.lbl_selected_heroes.configure(text=self.get_selected_heroes())
 
     def clicked_add_delete_monsters(self):
         self.update_selected_monsters()
-        self.lbl_selected_monsters.configure(text=self.get_selected(self.selected_monsters))
+        self.lbl_selected_monsters.configure(text=self.get_selected_monsters())
 
 class MainWindow(Frame):
     def __init__(self, master=None):
@@ -121,15 +133,61 @@ class MainWindow(Frame):
 
         self.heroes = Heroes()
         self.selected_heroes = self.heroes.get_names_heroes()
+        self.battle_heroes = self.selected_heroes
 
         self.monsters = Monsters()
         self.selected_monsters = {}
+        self.battle_monsters = {}
+
+        self.los = Listbox()
+        for i in self.selected_heroes:
+            self.los.insert(0, i)
+
+        self.los.bind('<<ListboxSelect>>', self.on_select)
+        self.los.bind("<Double-Button-1>", self.on_double_click)
+        self.los.grid(column=1, row=0)
+
+    def on_double_click(self, event):
+        w = InfoWindowHero(self.los.get(self.los.curselection()))
+
+
+    def on_select(self, event):
+        # los.curselection() - получение индекса выделенного элемента
+        # los.get() - получение элемента по его индексу
+        print(self.los.get(self.los.curselection()))
 
 
     def clicked(self):
+        self.los.delete(0, len(self.battle_heroes))
         w = SettingsWindow(self.selected_monsters, self.selected_heroes)
         w.wait_window()
+        self.battle_heroes = w.selected_heroes
+        self.battle_monsters = w.selected_monsters
+
+        for i in self.battle_heroes:
+            self.los.insert(0, i)
         print('Selected:', w.selected_monsters, w.selected_heroes)
+
+class InfoWindowHero(Toplevel):
+    def __init__(self, name):
+        Toplevel.__init__(self)
+        #self.master = master
+        #self.pack(fill=BOTH, expand=1)
+
+        self.lbl = Label(self, text=name, font=("Arial", 18))
+        self.lbl.grid(column=3, row=0)
+
+        self.hero = Heroes().get_info_hero_for_name(name)
+        print(self.hero)
+        #self.kd = self.heroes['heros'][]
+
+        self.text = Entry(self)
+        self.text.insert(0, str(self.hero['KD']))
+        self.text.grid(column=4, row=0)
+
+
+
+
 
 
 
